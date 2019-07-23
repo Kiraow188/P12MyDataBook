71b2ef85bd957a28be30c8db89955751054f6aad
Kiraow188 <17010368@myrp.edu.sg>
2019-07-23 15:38:42 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/p12_mydatabook/AnniversaryFragment.java
@@ -0,0 +1,74 @@
+package sg.edu.rp.c347.p12_mydatabook;
+
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AnniversaryFragment extends Fragment {
+
+    Button btn;
+    TextView tv;
+
+    public AnniversaryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.anniversaryfragment, container, false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+                Toast.makeText(getContext(), "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        tv = view.findViewById(R.id.TextView);
+        btn = view.findViewById(R.id.btnEdit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Edit Anniversary");
+                final EditText input = new EditText(getContext());
+                alert.setView(input);
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tv.setText(input.getText().toString());
+                    }
+                });
+                alert.show();
+            }
+        });
+
+
+        return view;    }
+
+}
